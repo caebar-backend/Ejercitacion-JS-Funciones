9d7e64977e36e5469a58e518c214a1a11d46551a
[caebar-backend] <[caebar.cursos.programacion@gmail.com]>
2025-04-13 22:55:49 -0300

    ejercicio03.js acabado

--- ejercicio03.js
+++ ejercicio03.js
@@ -1,3 +1,17 @@
 // Función F(X, Y):
 
 // Haga un programa en JavaScript que llame a la función F(X, Y) = 5.6 * X + Y / 2.5 y que muestre los resultados para F(3, 5); F(9, 2) y F(56, 87).
+
+const chalk = require('chalk')
+
+let X
+let Y
+
+const F = (X, Y) => {
+    return 5.6 * X + Y / 2.5
+}
+
+console.log(chalk.bgGreen(` Resultado de F(3, 5) = ${F(3, 5)}    `))
+console.log(chalk.bgGray(` Finalizacion para F(9, 2) = ${F(9, 2)}   `))
+console.log(chalk.bgBlue(` Resolución de la operación F(56, 87) = ${F(56, 87)} `))
+

6be9464dc7051f76c0279fc6d7b081ca8751349a
[caebar-backend] <[caebar.cursos.programacion@gmail.com]>
2025-04-13 23:05:24 -0300

    ejercicio04.js solucionado

--- ejercicio04.js
+++ ejercicio04.js
@@ -1,3 +1,24 @@
 // Conversión de minúscula a mayúscula:
 
-// Escriba un programa en JavaScript que utilice una función que reciba un carácter como parámetro, si el carácter es una letra minúscula, modifique el parámetro para convertirlo en mayúscula.
+// Escriba un programa en JavaScript que utilice una función que reciba un carácter como parámetro,
+//  si el carácter es una letra minúscula, modifique el parámetro para convertirlo en mayúscula.
+
+const prompt = require('prompt-sync')({sigint: true})
+const chalk = require('chalk')
+
+const parametroMayuscula = (caracter) => {
+    if(isNaN(caracter)){
+    return caracter.toUpperCase()
+    }
+    else{
+    return "No es una letra lo que escribió!!!!!!!"
+    }
+}
+
+const ingreso = prompt('Ingrese un caracter no numérico en minúscula: ----> ')
+
+const ingresoTransformado = ingreso.toLowerCase()
+
+const resultado = parametroMayuscula(ingresoTransformado)
+
+console.log(chalk.bgGreen(' ---> '+ ' El caracter transformado a MAYÚSCULAS es ----> ' + resultado + ' <--- '))

ad47f976508e90cd3557a5cef4eafdf0e7b29394
[caebar-backend] <[caebar.cursos.programacion@gmail.com]>
2025-04-13 23:20:24 -0300

    ejercicio05.js concluido

--- ejercicio05.js
+++ ejercicio05.js
@@ -1,3 +1,32 @@
 // Factorial de un número:
 
 // Escriba un programa en JavaScript con una función que reciba un valor entero positivo y devuelva su factorial.
+
+const prompt = require('prompt-sync')({sigint: true})
+const chalk = require('chalk')
+
+const calcularFactorial = (numero) => {
+    if(numero === 0 || numero === 1){
+        return 1
+    }
+    if(isNaN(numero)){
+        return "Error: El valor ingresado no es un número entero."
+    }
+    if(numero < 0){
+        return "Error: El valor ingresado no es un número entero positivo."
+    }
+
+   let factorial = 1
+   for(let i = 2; i <= numero; i++){
+    factorial *= i
+   }
+   return factorial
+}
+
+const pregunta = parseInt(prompt(chalk.greenBright(' Escriba un número para calcular el factorial ---> ')))
+
+const resultado = calcularFactorial(pregunta)
+console.log(chalk.blueBright(`<--- El factorial de ${pregunta} es ${resultado} --->`))
+
+
+

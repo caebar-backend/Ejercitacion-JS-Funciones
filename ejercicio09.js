fffe647ac5232d9d1d50a118e172d0ce0b2d7008
[caebar-backend] <[caebar.cursos.programacion@gmail.com]>
2025-04-16 21:06:02 -0300

    ejercicio09.js realizado

--- ejercicio09.js
+++ ejercicio09.js
@@ -8,3 +8,193 @@
 // NumeroACadena: acepta un parámetro entero número entre 0 y 10, y devuelve una cadena de caracteres que representa el número en letras.
 // Intercambio: acepta dos parámetros pasados por referencia que intercambian los valores de los parámetros.
 // Pida al usuario dos números y muestre la "distancia" entre ellos (el valor absoluto de su diferencia, de modo que el resultado sea siempre positivo).
+
+const prompt = require('prompt-sync')({sigint: true})
+const chalk = require('chalk')
+
+const calcularDistanciaNumeros = (uno, dos) => {
+
+    if(isNaN(uno) || isNaN(dos)){
+        console.log(chalk.red("Error: Los valores ingresados no son números."))
+        menuPrincipal()
+    }
+    if(uno < 0 || dos < 0){
+        console.log(chalk.red("Error: Los números deben ser positivos."))
+        menuPrincipal()
+    }
+
+    const final = Math.abs(uno - dos) - 1
+    console.log(chalk.bgGray('La distancia entre los números es: ' + "-- " + final + " --" + ' unidades entre medio de los dos'))
+    menuPrincipal()
+}
+
+const intercambioValores= (valorUno, valorDos) => {
+
+    const objetoValores = []
+
+    objetoValores.push({
+            "ValorUno": valorUno, 
+            "ValorDos": valorDos,
+    })
+
+    console.log(" Los valores originales son: ")
+    console.log(objetoValores)
+
+   const cambioUno = objetoValores[0].ValorUno
+   const cambioDos = objetoValores[0].ValorDos
+
+   objetoValores.splice(0)
+
+   objetoValores.push({
+    "ValorUno": cambioDos,
+    "ValorDos": cambioUno,
+   })
+    console.log('')
+    console.log(" Los valores alternados son: ")
+    console.log(objetoValores)
+
+    menuPrincipal()
+
+
+
+}
+
+const numeroACadena = (numero) => {
+    const numerosEnletras = ["cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez"]
+
+    if(isNaN(numero)){
+        console.log(chalk.redBright(` Error : El valor ingresado no es un número!!! `))
+        menuPrincipal()
+    }
+    if(numero < 0){
+        console.log(chalk.redBright(` Error : El valor ingresado no es positivo ni es cero!! `))
+        menuPrincipal()
+    }
+    if(numero > 10){
+        console.log(chalk.redBright(` Error : El valor ingresado no es menor o igual a 10!!! `))
+        menuPrincipal()
+    }
+    
+    const numeroElegido = numerosEnletras[numero]
+    const resolucion = numeroElegido.toUpperCase()
+
+    console.log(chalk.greenBright(` El número ingresado se denomina ---> " ${resolucion} " <--- `))
+    menuPrincipal()
+    
+
+
+
+
+}
+
+const esPar = (parametroAnalizar) => {
+    
+    if(isNaN(parametroAnalizar)){
+        console.log(chalk.redBright(' Error: El valor ingresado no es un número!!! '))
+        menuPrincipal()
+    }
+    if(parametroAnalizar <= 0){
+        console.log(chalk.redBright(` Error: El valor ingresado debe ser un número entero positivo " MAYOR a 0 "!!! `))
+        menuPrincipal()
+    }
+
+    const final = parametroAnalizar % 2
+
+    if(final === 0){
+        console.log(chalk.magenta(` El número ${parametroAnalizar} es par!!!. Finalización ----> VERDADERO `))
+        menuPrincipal()
+    }else{
+        console.log(chalk.magenta(` El número ${parametroAnalizar} es impar!!!. Resolución ----> FALSO `))
+        menuPrincipal()
+    }
+
+
+}
+
+const resto = (parametroUno, parametroDos) => {
+    const numeroUno = parametroUno
+    const numeroDos = parametroDos
+    
+    if(isNaN(parametroUno) || isNaN(parametroDos)){
+        console.log(chalk.redBright(" Error: Los parámetros deben ser números enteros! "))
+        menuPrincipal()
+        
+    }
+    if(parametroUno < 0 || parametroDos < 0){
+        console.log(chalk.redBright(" Error: Los parámetros deben ser números enteros positivos! "))
+        menuPrincipal()
+    }
+
+    const resultadoUno = parametroUno - parametroDos * Math.floor(parametroUno / parametroDos)
+    const resultadoDos = numeroDos - numeroUno * Math.floor(numeroDos / numeroUno)
+
+    console.log(chalk.greenBright(` El resto de la división entera entre "${numeroUno}" dividido "${numeroDos}" es "${resultadoUno}" `))
+    console.log(chalk.greenBright(` El resto de la división entera entre "${parametroDos}" dividido "${parametroUno}" es "${resultadoDos}" `))
+
+    menuPrincipal()
+
+}
+
+const bienvenida = (parametro) => {
+    console.log(chalk.magentaBright(` ${parametro}, Bienvenid@ a mi programa de JavaScript! \n Espero que disfrutes tu estancia en esta apliación. \n ¡Que tengas un buen día!\n`))
+    menuPrincipal()
+
+}
+
+
+const menuPrincipal = () => {
+    console.log("\n")
+    console.log(chalk.yellowBright(" ¿Qué Desea Hacer?"))
+    console.log(chalk.yellowBright(" Indique el número de la opción que desee: "))
+    console.log(chalk.yellowBright(" || 1 - Menú || 2 - Bienvenida || 3 - Resto || 4 - EsPar || 5 - Número a Cadena || 6 - Intercambio || 7 - Distancia entre Números || 8 - Salir "))
+    const pregunta = prompt(chalk.yellowBright(" ---> "))
+     
+    const respuesta = parseInt(pregunta)
+
+    switch(respuesta){
+        case 1:
+            menuPrincipal()
+            break;
+            case 2:
+                const nombreApellido = prompt(chalk.cyanBright(" Ingrese su nombre y apellido: ")).toUpperCase()
+                bienvenida(nombreApellido)
+                break;
+                case 3: 
+                const parametroUno = parseInt(prompt(chalk.cyanBright(" Ingrese el primer número: ")))
+                const parametroDos = parseInt(prompt(chalk.cyanBright(" Ingrese el segundo número: ")))
+                resto(parametroUno,parametroDos)
+                break;
+                case 4:
+                    const numeroEsPar = parseInt(prompt(chalk.magentaBright(" Ingrese un número para analizar si es Par o nó, y obtener Verdadero o Falso como resultado : ")))
+                    esPar(numeroEsPar)
+                    break;
+                    case 5:
+                        const numeroCadena = parseInt(prompt(chalk.greenBright(" Ingrese un número para convertirlo a cadena letras: ")))
+                        numeroACadena(numeroCadena)
+                        break;
+                        case 6:
+                            const valorUno = prompt(chalk.cyanBright(" Ingrese el primer valor para intercambiar (Número, Simbolo, letra, etc): "))
+                            const valorDos = prompt(chalk.cyanBright(" Ingrese el segundo valor para intercambiar (Número, Símbolo, letra, etc): "))
+                            intercambioValores(valorUno,valorDos)
+                            break;
+                            case 7:
+                                const numONE = prompt(chalk.bgGray(" Ingrese un número para calcular la distancia entre el segundo "))
+                                const numTWO = prompt(chalk.bgGray(" Ingrese un número para calcular la distancia entre el primero "))
+                                calcularDistanciaNumeros(numONE, numTWO)
+                                break;
+                                
+                                case 8:
+                                console.log(chalk.redBright("¡Hasta luego!"))
+                                process.exit(0)
+                                break;
+
+                                default:
+                                    console.log(chalk.redBright("Opción no válida"))
+                                    menuPrincipal()
+                                    break;
+
+    }
+
+}
+
+menuPrincipal()
--- ejercicio10.js
+++ ejercicio10.js
@@ -3,3 +3,71 @@
 // Diseñe un algoritmo en JavaScript que permita mediante un menú de opciones:
 // Ingresar un número y obtener su correspondiente valor en la sucesión de Fibonacci.
 // Solicitar un número y determinar si pertenece a la sucesión de Fibonacci mediante la devolución de un valor lógico.
+
+const promptSync = require('prompt-sync')({sigint: true});
+
+const chalk = require('chalk');
+
+let arrayDeNumeros = []
+
+const esFibonacci = (numero) => {
+    let a = 0
+    let b = 1
+    let auxiliar = 0
+
+    while(b <= numero) {
+        if(b === numero) {
+            
+            return console.log(chalk.greenBright('Es fibonacci el número!!'))
+        }
+        auxiliar = a + b
+        a = b
+        b = auxiliar
+    }
+     console.log(chalk.redBright('No es fibonacci el número!!'))
+     process.exit(1)
+    
+}
+
+const fibonacciCalculo = (numero) => {
+    let numCero = 0
+    let numUno = 1
+    let numAux = 0
+
+    if(numero === 0 || numero === 1) {
+        return console.log(chalk.greenBright('El número 0 o 1 es fibonacci!!'))
+    }
+    
+   for(let i = 2; i < numero; ) {
+        
+        numAux = numCero + numUno
+        console.log(chalk.yellowBright('Numeros ---> ' + numCero + ' + ' + numUno+ ' + ' + ' = ' + numAux))
+        
+        if(numAux > numero){
+            arrayDeNumeros.push(numCero, numUno, numAux)
+            console.log(chalk.cyanBright(`En la sucesión de Fibonacci el número anterior "${arrayDeNumeros[0]}" más el número que escribiste "${arrayDeNumeros[1]}" es igual al siguiente número fibonacci "${arrayDeNumeros[2]}"`))
+            console.log(chalk.greenBright('Llegamos al final del calculo!!!!'))
+             return;
+             
+         }
+ 
+        numCero = numUno
+        numUno = numAux
+
+        i++
+
+    }
+    
+}
+
+const pregunta = parseInt(promptSync(chalk.cyanBright('Ingrese un número entero para controlar que sea parte de la sucesión de FIBONACCI ---> ')))
+
+if(isNaN(pregunta)){
+    console.log(chalk.redBright('Error: Debe ingresar un número entero!!!'))
+}
+if(pregunta <=0){
+    console.log(chalk.redBright('Error: Debe ingresar un número entero positivo!!!'))
+}
+
+esFibonacci(pregunta)
+fibonacciCalculo(pregunta)
